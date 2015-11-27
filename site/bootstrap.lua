c89fc0e758a48794900832e702a33315e1073bcd
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-11-27 13:19:51 +0200

    Renamed route to self.

--- site/bootstrap.lua
+++ site/bootstrap.lua
@@ -13,25 +13,25 @@ route:use "template"
 
 route:with "equals"
 
-route:get("/", function(route)
-    route:render "chat/join.html"
+route:get("/", function(self)
+    self:render "chat/join.html"
 end)
 
-route:post("/", function(route)
-    local context = route.context
+route:post("/", function(self)
+    local context = self.context
     local redis = context.redis
     local valid, form, _ = validate.chat.join(context.post)
     if valid then
         local nick, e = db.nicks.register(redis, form.nick.value)
-        if not nick then route:error(e) end
+        if not nick then self:error(e) end
         local nicks, e = db.nicks.all(redis)
-        if not nicks then route:error(e) end
+        if not nicks then self:error(e) end
         context.nick = nick
         context.nicks = nicks
-        route:render "chat/channel.html"
+        self:render "chat/channel.html"
     else
         context.form = form
-        route:to "/"
+        self:to "/"
     end
 end)
 
@@ -41,12 +41,12 @@ route:websocket "/" {
     text    = chat.text
 }
 
-route:get("/emojis.json", function(route)
-    route:json(autocomplete())
+route:get("/emojis.json", function(self)
+    self:json(autocomplete())
 end)
 
-route:post("/emojis.json", function(route)
-    route:json(autocomplete(route.context.post.search, 5))
+route:post("/emojis.json", function(self)
+    self:json(autocomplete(self.context.post.search, 5))
 end)
 
 route:dispatch()
