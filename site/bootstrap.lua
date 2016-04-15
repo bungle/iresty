68d6132f6af23d2ff08d41a0f76d6552145eb032
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2016-04-15 14:50:00 +0300

    Updated lua-resty-route and made changes accordingly.

--- site/bootstrap.lua
+++ site/bootstrap.lua
@@ -10,13 +10,11 @@ route:use "redis" {}
 route:use "reqargs" {}
 route:use "template"
 
-route:with "equals"
-
-route:get("/", function(self)
+route:get("=/", function(self)
     self:render "chat/join.html"
 end)
 
-route:post("/", function(self)
+route:post("=/", function(self)
     local context = self.context
     local redis = context.redis
     local valid, form, _ = validate.chat.join(context.post)
@@ -34,13 +32,13 @@ route:post("/", function(self)
     end
 end)
 
-route:websocket "/" "app.chat"
+route:websocket "=/" "app.chat"
 
-route:get("/emojis.json", function(self)
+route:get("=/emojis.json", function(self)
     self:json(autocomplete())
 end)
 
-route:post("/emojis.json", function(self)
+route:post("=/emojis.json", function(self)
     self:json(autocomplete(self.context.post.search, 5))
 end)
 
