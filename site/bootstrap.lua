2248408db069458425d3e20cf1448a1005534248
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2016-04-23 22:48:10 +0300

    Removed unneeded middleware.

--- site/bootstrap.lua
+++ site/bootstrap.lua
@@ -2,9 +2,7 @@ local route        = require "resty.route".new()
 local validate     = require "app.validate".chat.join
 local nicks        = require "app.db".nicks
 local autocomplete = require "app.emojis".autocomplete
-
-route:use "ajax"
-route:use "pjax"
+Removed
 route:use "form"
 route:use "redis" {}
 route:use "reqargs" {}
